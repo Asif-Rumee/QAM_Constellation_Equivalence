806b37edf1f4c4cb46a8b2b6e0f821429a15657d
Asif Rahman Rumee <52595325+Asif-Rumee@users.noreply.github.com>
2021-12-31 18:15:29 +0600

    Add files via upload

---
+++ two_one.cpp
@@ -0,0 +1,150 @@
+/*
+A simple program to map from binary sequences to complex numbers and vice-versa.
+Author: Asif Rahman Rumee
+Date: 18/12/2021
+
+*/
+
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class point{
+    float x_cord, y_cord;
+public:
+    point(float x=0, float y=0){
+        x_cord = x;
+        y_cord = y;
+    }
+    float get_x(){
+        return x_cord;
+    }
+    float get_y(){
+        return y_cord;
+    }
+    void display(){
+        if(y_cord == 1){
+            cout<< x_cord << " + " << "j"<<endl;
+        }else if(y_cord == -1){
+            cout<< x_cord << " - " << "j"<<endl;
+        }else if(y_cord < 0){
+            cout<< x_cord << " - " << y_cord * -1 << "j"<<endl;
+        }
+        else{
+            cout<< x_cord << " + " << y_cord << "j"<<endl;
+        }
+    }
+};
+
+class complex_binary_alter{
+    point a, n, r, b, points_list[16];
+    short a_bits[4], b_bits[4];
+public:
+    void create_r(){
+        unsigned seed = chrono::steady_clock::now().time_since_epoch().count();
+        default_random_engine e (seed);
+        normal_distribution<double> dist_gauss(0,sqrt(4));
+
+        r = point(a.get_x() + dist_gauss(e), a.get_y() + dist_gauss(e));
+        cout<<"point r: ";
+        r.display();
+    }
+
+    short demap_b(){
+        create_a();
+        create_r();
+        b = find_minimum(r);
+        cout<<"point of min distance: ";
+        b.display();
+
+        b.get_x() > 0 ? b_bits[0] = 0 : b_bits[0] = 1;
+        b.get_y() > 0 ? b_bits[1] = 0 : b_bits[1] = 1;
+        abs(b.get_x()) == 1 ? b_bits[2] = 0 : b_bits[2] = 1;
+        abs(b.get_y()) == 1 ? b_bits[3] = 0 : b_bits[3] = 1;
+        cout<< "binary sequence of b: ";
+        for(int i=0; i<4; i++){
+            cout<< b_bits[i];
+        }
+        cout<< endl;
+
+        cout<< "bit differences: " << count_bit_differences();
+        return count_bit_differences();
+    }
+
+    void create_a(){
+        short sign_real, sign_imaginary, digit_real, digit_imaginary;
+        srand(time(NULL));
+        for(int i=0; i<4; i++){
+            a_bits[i] = rand()%2;
+        }
+        cout<< "binary sequence of a: ";
+        for(int i=0; i<4; i++){
+            cout<< a_bits[i];
+        }
+        cout<< endl;
+
+        a_bits[0] == 0 ? sign_real = 1 : sign_real = -1;
+        a_bits[1] == 0 ? sign_imaginary = 1 : sign_imaginary = -1;
+        a_bits[2] == 0 ? digit_real = 1 : digit_real = 3;
+        a_bits[3] == 0 ? digit_imaginary = 1 : digit_imaginary = 3;
+
+        a = point(sign_real*digit_real, sign_imaginary*digit_imaginary);
+        cout<<"point a: ";
+        a.display();
+    }
+
+    float find_distance(point p, point q){
+        return sqrt((p.get_x() - q.get_x())*(p.get_x() - q.get_x()) + (p.get_y() - q.get_y())*(p.get_y() - q.get_y()));
+    }
+
+    void create_points_list(){
+        int index = 0;
+        int sign_x[4] = {1,1,-1,-1};
+        int sign_y[4] = {1,-1,1,-1};
+        for(int i=1; i<4; i+=2){
+            for(int j=1; j<4; j+=2){
+                for(int k=0; k<4; k++){
+                    this->points_list[index] = point(i*sign_x[k], j*sign_y[k]);
+                    index++;
+                }
+            }
+        }
+    }
+
+    point find_minimum(point p){
+        create_points_list();
+        float minimum = find_distance(p, points_list[0]);
+        int min_idx = 0;
+
+        for(int i=1; i<16; i++){
+            if(minimum > find_distance(p, points_list[i])){
+                minimum = find_distance(p, points_list[i]);
+                min_idx = i;
+            }
+        }
+        return points_list[min_idx];
+    }
+
+    short count_bit_differences(){
+        short counts = 0;
+
+        for(int i=0; i<4; i++){
+            if(a_bits[i] != b_bits[i]){
+                counts++;
+            }
+        }
+        return counts;
+    }
+};
+
+main(){
+int sum = 0;
+for(int i=1; i<=100; i++){
+    complex_binary_alter trial;
+    sum += trial.demap_b();
+    cout<<endl<<endl;
+}
+cout<< "total bit differences: " << sum;
+return 0;
+}
